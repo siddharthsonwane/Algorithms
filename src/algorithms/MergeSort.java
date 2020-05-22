f5073b67a5777f5366ca246e0bca40bf16cce028
siddharth1609 <siddharthsonwane18@gmail.com>
2020-05-22 12:38:03 +0530

    Merge sort added

---
+++ src/algorithms/MergeSort.java
@@ -0,0 +1,105 @@
+package algorithms;
+
+public class MergeSort {
+
+	
+	private static void printArray(int[] arr) {
+
+		int n = arr.length;
+		
+		for(int i =0; i < n; i ++) {
+          System.out.println(arr[i] + "");
+		}
+		
+		
+		
+	}
+
+    private void sort(int[] arr, int l, int r) {
+
+	   if(l < r){
+
+	   	int mid = (l+r)/2;
+
+	   	sort(arr, l,mid);
+
+	   	sort(arr ,mid +1, r);
+
+
+        merge(arr,l,mid,r);
+
+	   }
+
+}
+
+	private void merge(int[] arr, int l, int m, int r) {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        /* Create temp arrays */
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        /*Copy data to temp arrays*/
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+
+        /* Merge the temp arrays */
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarry array
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        /* Copy remaining elements of L[] if any */
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+
+        int arr[] = {12, 11, 13, 5, 6, 7};
+
+        System.out.println("Given Array");
+        printArray(arr);
+
+        MergeSort ob = new MergeSort();
+        ob.sort(arr, 0, arr.length-1);
+
+        System.out.println("\nSorted array");
+        printArray(arr);
+    }
+
+
+
+
+	
+
+}
